9e19abe981ef5b06a04ee1140ef91b538e550ff1
KHYehor <voodo1203@gmail.com>
2019-10-25 01:00:51 +0300

    something workable

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,121 +1,80 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Menu} from 'semantic-ui-react';
+import {useAuth0} from '../react-auth0-spa.js';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
-  Redirect,
-  useHistory,
-  useLocation,
+  // Redirect,
+  // useHistory,
+  // useLocation,
 } from 'react-router-dom';
 
-export default function AuthExample() {
-  return (
-    <Router>
-      <div>
-        <AuthButton />
+export default function MenuExampleSecondaryPointing() {
+  const [activeItem, setActiveItem] = useState('home');
+  const all = useAuth0();
+  const {isAuthenticated, loginWithRedirect, logout, loading} = all;
+  console.dir(all, {showHidden: true});
 
-        <ul>
-          <li>
-            <Link to="/public">Public Page</Link>
-          </li>
-          <li>
-            <Link to="/protected">Protected Page</Link>
-          </li>
-        </ul>
+  const handleItemClick = (e, {name}) => setActiveItem(name);
+  const LogIn = () => (isAuthenticated ? logout() : loginWithRedirect({}));
 
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <div>
+      <Router>
+        <Menu pointing secondary>
+          <Link to="/home">
+            <Menu.Item
+              name="home"
+              active={activeItem === 'home'}
+              onClick={handleItemClick}
+            />
+          </Link>
+          <Link to="messages">
+            <Menu.Item
+              name="messages"
+              active={activeItem === 'messages'}
+              onClick={handleItemClick}
+            />
+          </Link>
+          <Link to="/friends">
+            <Menu.Item
+              name="friends"
+              active={activeItem === 'friends'}
+              onClick={handleItemClick}
+            />
+          </Link>
+          <Menu.Menu position="right">
+            <Link to="">
+              <Menu.Item
+                name={isAuthenticated ? 'Log out' : 'Log in'}
+                active={activeItem === 'login'}
+                onClick={LogIn}
+              />
+            </Link>
+          </Menu.Menu>
+        </Menu>
         <Switch>
-          <Route path="/public">
-            <PublicPage />
+          <Route path="/home">
+            <h1>Привет, подстригись</h1>
+          </Route>
+          <Route path="/messages">
+            <h1>Привет, подстригись</h1>
+          </Route>
+          <Route path="/friends">
+            <h1>Привет, подстригись</h1>
           </Route>
           <Route path="/login">
-            <LoginPage />
+            <h1>Привет, авторизированный</h1>
           </Route>
-          <PrivateRoute path="/protected">
-            <ProtectedPage />
-          </PrivateRoute>
+          <Route path="/logout">
+            <h1>Привет, авторизируйся снова)</h1>
+          </Route>
         </Switch>
-      </div>
-    </Router>
-  );
-}
-
-const fakeAuth = {
-  isAuthenticated: false,
-  authenticate(cb) {
-    fakeAuth.isAuthenticated = true;
-    setTimeout(cb, 100); // fake async
-  },
-  signout(cb) {
-    fakeAuth.isAuthenticated = false;
-    setTimeout(cb, 100);
-  },
-};
-
-function AuthButton() {
-  let history = useHistory();
-
-  return fakeAuth.isAuthenticated ? (
-    <p>
-      Welcome!{' '}
-      <button
-        onClick={() => {
-          fakeAuth.signout(() => history.push('/'));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  ) : (
-    <p>You are not logged in.</p>
-  );
-}
-
-// A wrapper for <Route> that redirects to the login
-// screen if you're not yet authenticated.
-function PrivateRoute({children, ...rest}) {
-  return (
-    <Route
-      {...rest}
-      render={({location}) =>
-        fakeAuth.isAuthenticated ? (
-          children
-        ) : (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: {from: location},
-            }}
-          />
-        )
-      }
-    />
-  );
-}
-
-function PublicPage() {
-  return <h3>Public</h3>;
-}
-
-function ProtectedPage() {
-  return <h3>Protected</h3>;
-}
-
-function LoginPage() {
-  let history = useHistory();
-  let location = useLocation();
-
-  let {from} = location.state || {from: {pathname: '/'}};
-  let login = () => {
-    fakeAuth.authenticate(() => {
-      history.replace(from);
-    });
-  };
-
-  return (
-    <div>
-      <p>You must log in to view the page at {from.pathname}</p>
-      <button onClick={login}>Log in</button>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App.js';
+import {Auth0Provider} from './react-auth0-spa.js';
+import config from './auth_config.json';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname,
+  );
+};
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById('root'),
+);
